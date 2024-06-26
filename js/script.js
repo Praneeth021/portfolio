f886bc23facc9799b74019741babe81c9c0bb98f
Praneeth <kadampallypraneeth987@gmail.com>
2024-06-26 15:22:00 -0400

    Add contact form and few minor UI updates

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed("#typewriter-text", options);
     console.log(typed);
 });
+
+
+(function () {
+    emailjs.init({publickey:'q1R_ZvmI-TukCIw_r'}); // Replace with your user ID from EmailJS
+})();
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const serviceID = 'service_iu4r9wa'; // Replace with your service ID from EmailJS
+    const templateID = 'template_7esykyo'; // Replace with your template ID from EmailJS
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            alert('Message sent successfully!');
+        }, (err) => {
+            alert(JSON.stringify(err));
+        });
+});
